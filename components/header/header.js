4156815bc3610432feed2d17e536cedc71f9148d
Sofia <snsandoval@scl.edu.gt>
2025-09-16 09:58:27 -0600

    Sistema de Carpetas

---
+++ components/header/header.js
@@ -0,0 +1,10 @@
+function header() {
+
+    let header = document.createElement ('header');
+    header.className = "header";
+
+    let titulo = document.createElement('h2');
+    titulo.className = "title";
+    titulo.textContent = "Lista de Compras";
+    
+}
